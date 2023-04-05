cad4dbf005be86b8878988a96f5c139a8745f4f9
anandgitacc <anand.akmail@gmail.com>
2023-04-05 17:37:34 +0530

    firstgit

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,33 @@
+package org.employee;
+
+
+public class EmployeeDetails {
+	
+	private void empName() {
+		// Business logic
+		System.out.println("Employee name: anand");
+	}
+	
+	private void empMobile() {
+		System.out.println("Employee mobile: 8939278836");
+	}
+	
+	private void empID() {
+		System.out.println("Employee id: 111");
+	}
+	
+	public static void main(String[] args) {
+		
+		EmployeeDetails e = new EmployeeDetails();
+		
+		// method call
+		e.empName();
+		e.empMobile();
+		e.empID();
+		
+	}
+	
+	
+	
+
+}
